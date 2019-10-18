30eccc192bff36646904b793421cc85f7b836cb0
George Williamson <keatonwilliamson@Georges-MacBook-Pro.local>
2019-10-17 19:46:36 -0500

    absol is moving

--- index.js
+++ index.js
@@ -7,46 +7,129 @@ var config = {
     type: Phaser.AUTO,
     width: 1200,
     height: 960,
+    backgroundColor: '#1b1464',
     parent: 'game',
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update: update
     },
     physics: {
-        default: "matter",
+        default: 'matter',
         matter: {
-            // debug: true
+            gravity: {
+                x: 0,
+                y: 0
+            }
         }
-    }
+    },
 };
 
+
+// var game = new Phaser.Game(config);
+
+// function preload() {
+//     // Load sprite sheet generated with TexturePacker
+//     this.load.atlas('sheet', 'assets/fruit-sprites.png', 'assets/fruit-sprites.json');
+
+//     // Load body shapes from JSON file generated using PhysicsEditor
+//     this.load.json('shapes', 'assets/fruit-shapes.json');
+// }
+
+// function create() {
+//     var shapes = this.cache.json.get('shapes');
+
+//     this.matter.world.setBounds(0, 0, game.config.width, game.config.height);
+//     this.add.image(0, 0, 'sheet', 'background').setOrigin(0, 0);
+
+//     // sprites are positioned at their center of mass
+//     var ground = this.matter.add.sprite(0, 0, 'sheet', 'ground', {shape: shapes.ground});
+//     ground.setPosition(0 + ground.centerOfMass.x, 280 + ground.centerOfMass.y);  // corrected position: (0,280)
+
+//     this.matter.add.sprite(200, 50, 'sheet', 'crate', {shape: shapes.crate});
+//     this.matter.add.sprite(250, 250, 'sheet', 'banana', {shape: shapes.banana});
+//     this.matter.add.sprite(360, 50, 'sheet', 'orange', {shape: shapes.orange});
+//     this.matter.add.sprite(400, 250, 'sheet', 'cherries', {shape: shapes.cherries});
+
+//     this.input.on('pointerdown', function (pointer) {
+//         this.matter.add.sprite(pointer.x, pointer.y, 'sheet', 'banana', {shape: shapes.banana});
+//     }, this);
+// }
+
+
+// var config = {
+//     type: Phaser.AUTO,
+//     width: 800,
+//     height: 600,
+
+//     parent: 'phaser-example',
+//     physics: {
+//         default: 'matter',
+//         matter: {
+//             gravity: {
+//                 x: 0,
+//                 y: 0
+//             }
+//         }
+//     },
+//     scene: {
+//         preload: preload,
+//         create: create,
+//         update: update
+//     }
+// };
+
+var absol;
+var cursors;
 
 var game = new Phaser.Game(config);
 
 function preload() {
-    // Load sprite sheet generated with TexturePacker
-    this.load.atlas('sheet', 'assets/fruit-sprites.png', 'assets/fruit-sprites.json');
-
-    // Load body shapes from JSON file generated using PhysicsEditor
-    this.load.json('shapes', 'assets/fruit-shapes.json');
+    this.load.image('absol', 'assets/mega-absol.png');
 }
 
 function create() {
-    var shapes = this.cache.json.get('shapes');
+    absol = this.matter.add.image(400, 300, 'absol');
 
-    this.matter.world.setBounds(0, 0, game.config.width, game.config.height);
-    this.add.image(0, 0, 'sheet', 'background').setOrigin(0, 0);
+    absol.setFixedRotation();
+    absol.setAngle(0);
+    absol.setFrictionAir(0.05);
+    absol.setMass(30);
 
-    // sprites are positioned at their center of mass
-    var ground = this.matter.add.sprite(0, 0, 'sheet', 'ground', {shape: shapes.ground});
-    ground.setPosition(0 + ground.centerOfMass.x, 280 + ground.centerOfMass.y);  // corrected position: (0,280)
+    this.matter.world.setBounds(0, 0, 1200, 960);
 
-    this.matter.add.sprite(200, 50, 'sheet', 'crate', {shape: shapes.crate});
-    this.matter.add.sprite(250, 250, 'sheet', 'banana', {shape: shapes.banana});
-    this.matter.add.sprite(360, 50, 'sheet', 'orange', {shape: shapes.orange});
-    this.matter.add.sprite(400, 250, 'sheet', 'cherries', {shape: shapes.cherries});
+    cursors = this.input.keyboard.createCursorKeys();
 
-    this.input.on('pointerdown', function (pointer) {
-        this.matter.add.sprite(pointer.x, pointer.y, 'sheet', 'banana', {shape: shapes.banana});
-    }, this);
+    // cursors = this.input.keyboard.createCursorKeys();
+    // yeahh = this.input.on('pointerdown', function (pointer) {
+    //     console.log(this.input)
+    //     // this.matter.add.sprite(pointer.x, pointer.y, 'sheet', 'banana', { shape: shapes.banana });
+    // }, this);
+
+}
+
+function update() {
+    // absol.thrustBack(0.1);
+    // this.input.on('pointerdown', function (pointer) {
+    //         console.log(this.input)
+
+    // })
+    
+    if (cursors.left.isDown) {
+        absol.thrustBack(0.1);
+        console.log("yeah")
+    }
+    else if (cursors.right.isDown) {
+        absol.thrust(0.1);
+        console.log("yeah")
+    }
+
+    if (cursors.up.isDown) {
+        absol.thrustLeft(0.1);
+        console.log("yeah")
+    }
+    else if (cursors.down.isDown) {
+        absol.thrustRight(0.1);
+        console.log("yeah")
+    }
 }
